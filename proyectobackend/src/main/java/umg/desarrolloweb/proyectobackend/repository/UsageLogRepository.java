31085d0779b18f53153781d35c04ced0af8c33ab
JDavOM26 <135679885+JDavOM26@users.noreply.github.com>
2025-10-20 17:54:46 -0600

    Update UsageLogRepository.java

--- proyectobackend/src/main/java/umg/desarrolloweb/proyectobackend/repository/UsageLogRepository.java
+++ proyectobackend/src/main/java/umg/desarrolloweb/proyectobackend/repository/UsageLogRepository.java
@@ -13,7 +13,7 @@ import umg.desarrolloweb.proyectobackend.entity.UsageLog;
 public interface UsageLogRepository extends JpaRepository<UsageLog, Integer> {
 	@Query(value = "SELECT t.event_type, COALESCE(COUNT(u.event_type), 0) AS count " +
 	         "FROM (SELECT 0 AS event_type UNION SELECT 1 UNION SELECT 2) t " +
-	         "LEFT JOIN usage_log u ON u.event_type = t.event_type " +
+	         "LEFT JOIN USAGE_LOG u ON u.event_type = t.event_type " +
 	         "AND u.timestamp BETWEEN :startDate AND :endDate " +
 	         "GROUP BY t.event_type " +
 	         "ORDER BY t.event_type", nativeQuery = true)
